9c945b8fb3c901343c28de1c5eddeaee97c07b6b
Sohair Ahmad <sohair1991@gmail.com>
2016-02-11 12:19:15 +0500

    SetUp the Chat Channel Server side

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,13 +1,23 @@
-# Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 class ChatChannel < ApplicationCable::Channel
+
   def subscribed
-    # stream_from "some_channel"
+    stream_from 'messages'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast('messages',
+      message: render_message(data['message']))
   end
+
+  private
+
+  def render_message(message)
+    ApplicationController.render(partial: 'messages/message',
+                                 locals: { message: message })
+  end
+
 end
